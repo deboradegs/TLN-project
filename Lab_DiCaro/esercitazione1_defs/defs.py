333b4886fd52f2a7d617a52f4492d8170dc2277b
saradamonte <58686024+saradamonte@users.noreply.github.com>
2022-11-08 16:13:51 +0100

    Update defs.py

--- Lab_DiCaro/esercitazione1_defs/defs.py
+++ Lab_DiCaro/esercitazione1_defs/defs.py
@@ -8,46 +8,37 @@ from collections import Counter
 from tabulate import tabulate
 import heapq
 
-
-
 def data_to_dict():
     return pd.read_excel('../resources/definizioni.xlsx', index_col=0).T.to_dict('list')
 
 def stem_lem(text):
-    lemmatizer = WordNetLemmatizer()
+    #lemmatizer = WordNetLemmatizer()
     stemming = SnowballStemmer('english')
     stop_words = stopwords.words('english')
     stop_words.extend(['someone', 'something', 'e', 'e.g', 'u', 'ha', 'e.i', 'others', "'s"])
     string_punctuation = string.punctuation + '’'
     words = list()
     for word in word_tokenize(str(text).lower()):
-        lemma = lemmatizer.lemmatize(word)
-        stemma = stemming.stem(lemma)
+        #lemma = lemmatizer.lemmatize(word)
+        stemma = stemming.stem(word)
         if stemma not in stop_words and stemma not in string_punctuation:
             words.append(stemma)
-    #print(words)
     return words
     
 def frequency(dictionary: dict, num_common_word):
     frequency = dict()
-    score_list = dict()
     for concept, definitions in dictionary.items():
         stemma_list = list()
         for definition in definitions:
-            #print(definition)
             stemma_list.extend(stem_lem(definition))
-        #print(stemma_list)
         counts = Counter(stemma_list)
         frequency[concept] = heapq.nlargest(num_common_word, counts, key=counts.get)
-        #print(counts)
     return frequency
 
 
 def word_in_sentence(dictionary: dict, frequency: dict):
     concept_score = dict()
-    
     for concept1, words in frequency.items():
-        score_list = dict()
         sum = 0
         for word in words:
             counter = 0
@@ -57,16 +48,14 @@ def word_in_sentence(dictionary: dict, frequency: dict):
                     for definition in cleaned_definitions:
                         if str(word) in str(definition):
                             counter+=1
-            score_list[word] = dict({'word': word,
-                                'score': counter/len(definitions)
-                                })
-            
+            # score_list[word] = dict({'word': word,
+            #                     'score': counter/len(definitions)
+            #                     })
             sum += counter/len(definitions)
         #concept_score[concept1] = {'score': round(sum/len(words),3),
         #                           'score_list': dict(score_list) }
         concept_score[concept1] = round(sum/len(words),3)
         #dict(concept_score[concept1])
-    #print(concept_score)
     return concept_score
 
 # def overlap_words(dictionary: dict, word1, word2):
@@ -118,34 +107,36 @@ def word_in_sentence(dictionary: dict, frequency: dict):
 #         return 0
     
 def overlap_words(dictionary: dict, word1, word2):
-    sum = 0
-    my_words = {key: dictionary[key] for key in dictionary.keys()
-        & {word1, word2}}
-    frequents = frequency(my_words, 5)
-    scores = word_in_sentence(my_words, frequents)
+    sum = 0 
     for conc in scores:
-        sum +=scores[conc]
+        if conc == word1 or conc == word2:
+            sum+= scores[conc]
     return sum/2
 
-df_dict = data_to_dict()
 
+df_dict = data_to_dict()
 df_dict_freq = frequency(df_dict, 5)
+scores = word_in_sentence(df_dict, df_dict_freq)
+
+#---- Print ----
+
 table = pd.DataFrame.from_dict(df_dict_freq, orient='index')
+print()
+print('-------- The five most common words in each definition by concept --------')
+print()
 print(tabulate(table, headers=['Concept', 1, 2, 3, 4, 5], tablefmt='fancy_grid'))
 
-scores = word_in_sentence(df_dict, df_dict_freq)
-#for concept in scores:
-#    del scores[concept]['score_list']
-    #del concept['score_list']
 table2 = pd.DataFrame.from_dict(scores, orient='index')
 print()
 print()
+print('-------- Definitions overlap score for each concept --------')
+print()
 print(tabulate(table2, headers=['Concept', 'Score'], tablefmt='fancy_grid'))
 
-overlap_abstract = overlap_words(df_dict, 'Emotion', 'Revenge')
-overlap_concrete = overlap_words(df_dict, 'Person', 'Brick')
-overlap_generic = overlap_words(df_dict, 'Person', 'Emotion')
-overlap_specific = overlap_words(df_dict, 'Revenge', 'Brick')
+overlap_abstract = overlap_words(scores, 'Emotion', 'Revenge')
+overlap_concrete = overlap_words(scores, 'Person', 'Brick')
+overlap_generic = overlap_words(scores, 'Person', 'Emotion')
+overlap_specific = overlap_words(scores, 'Revenge', 'Brick')
 table3 =  [
     ['Concrete', overlap_concrete],
     ['Abstract', overlap_abstract],
@@ -153,6 +144,8 @@ table3 =  [
     ['Specific', overlap_specific]
 ]
 print()
+print()
+print('-------- Concept overlap score --------')
 print()
 print(tabulate(table3, headers=['Concepts grouped by', 'Score'], tablefmt='fancy_grid'))
 #print(scores)
